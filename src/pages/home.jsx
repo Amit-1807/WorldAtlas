dcd0ccd9545a3e222ba4035f39fb825372049508
Amit-1807 <150581997+Amit-1807@users.noreply.github.com>
2025-08-17 11:08:56 +0530

    create About page hero section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,11 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { HeroSection } from "../components/ui/heroSection";
+import { About } from "./about";
 export const Home = () => {
     return(
         <>
-        <h1>Home</h1>
+        <HeroSection/>
+        <About/>
         </>
     )
  
